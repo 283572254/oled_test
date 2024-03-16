d93c7158d072b9d6b955a93c71f740630f267371
283572254 <96158209+283572254@users.noreply.github.com>
2024-03-16 15:03:54 +0800

    需要复位才能重新开始游戏

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,8 @@
 
 #include <Arduino.h>
 
-
-
-#include <Arduino.h>
 #include <U8g2lib.h>
-
+#include <Wire.h>
 #ifdef U8X8_HAVE_HW_SPI
 #include <SPI.h>
 #endif
@@ -14,16 +11,18 @@
 #endif
 
 
-U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 18, /* data=*/ 19, /* reset=*/ U8X8_PIN_NONE);   // ESP32 Thing, pure SW emulated I2C
-
-
+//U8G2_SSD1306_128X64_NONAME_F_SW_I2C u8g2(U8G2_R0, /* clock=*/ 18, /* data=*/ 19, /* reset=*/ U8X8_PIN_NONE);   // ESP32 Thing, pure SW emulated I2C
+//U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ 4, /* data=*/ 5);   // ESP32 Thing, HW I2C with pin remapping
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0); // 使用硬件 I2C
 /*user define variable begin*/
 #define KEY0 0
 #define KEY1 1
-
+#define KEY2 2
+#define KEY3 6
 enum g_e_Dir {UP=1,DOWN,LEFT,RIGHT};
 bool g_b_food_eaten = true;
-bool g_b_key_statue = true;
+bool g_b_snake_statue = true;
+bool g_b_game_over = false;
 const uint8_t g_u8_Block[] PROGMEM = {
   0xf0, // B1111000
   0xb0, // B1011000
@@ -32,74 +31,59 @@ const uint8_t g_u8_Block[] PROGMEM = {
 };
 uint8_t g_u8_Snake_Headx = 4;
 uint8_t g_u8_Snake_Heady = 4;
-uint8_t g_u8_Snake_Len = 2;
+uint8_t g_u8_Snake_Len = 1;
 uint8_t g_u8_FoodX;
 uint8_t g_u8_FoodY;
-uint8_t g_u8_ArrayX[100];
-uint8_t g_u8_ArrayY[100];
-uint8_t g_u8_dir = 0;
+uint8_t g_u8_ArrayX[100] = {0};
+uint8_t g_u8_ArrayY[100] = {0};
+uint8_t g_u8_dir = DOWN;
 
 
 
 uint16_t g_u16_Score = 0;
 uint16_t g_u16_level = 1;
 uint16_t g_u16_SnakeSpeed =  150;
+
+int i;
 /*user define variable end*/
 
 /*user define function begin*/
-
+void check_snake_die(void);
 // 定义中断服务函数
 void  handleInterrupt_KEY0() {
-  
-  if(g_b_key_statue==true)
-  {
-      g_u8_dir = LEFT;
-  }
-  else
-  {
-      g_u8_dir = UP;
-  }
-  g_b_key_statue = !g_b_key_statue;
+    if(g_u8_dir != UP)
+      g_u8_dir = DOWN;
+    
 }
-
 void  handleInterrupt_KEY1() 
 {
-  if(g_b_key_statue==true)
-  {
-      g_u8_dir = RIGHT;
-  }
-  else
-  {
-      g_u8_dir = DOWN;
-  }
-  g_b_key_statue = !g_b_key_statue;
+  if(g_u8_dir != LEFT)
+    g_u8_dir = RIGHT;
+  
 }
-
+void  handleInterrupt_KEY2() {
+  if(g_u8_dir != DOWN)
+    g_u8_dir = UP;
+  
+}
+void  handleInterrupt_KEY3() {
+  if(g_u8_dir != RIGHT)
+    g_u8_dir = LEFT;
+  
+}
 void key_init()
 {
   pinMode(KEY0,INPUT_PULLUP);
   pinMode(KEY1,INPUT_PULLUP);
+  pinMode(KEY2,INPUT_PULLUP);
+  pinMode(KEY3,INPUT_PULLUP);
 }
 
-void key_scan()
-{
-  if(digitalRead(KEY0)==0)
-  {
-    delay(10);
-    if(digitalRead(KEY0)==0)
-      Serial.println("KEY0");
-  }
-  else if(digitalRead(KEY1)==0)
-  {
-    delay(10);
-    if(digitalRead(KEY1)==0)
-      Serial.println("KEY1");
-  }
-}
+
 
 void draw_snake(int x, int y)
 {
-  u8g2.drawBitmap(x, y, 1, 4, g_u8_Block);
+  u8g2.drawBox(x, y, 4, 4);
 }
 
 void show_score(int x, int y, int data)
@@ -120,8 +104,17 @@ void screen(void)
 
   show_score(110, 25, g_u16_level);
   show_score(110, 53, g_u16_Score);
-
-  for (int i = 0; i < g_u8_Snake_Len; i++)
+#if 0
+  if(g_b_snake_statue==true)
+  {
+    for (i = 0; i < g_u8_Snake_Len; i++)
+    {
+      draw_snake(g_u8_ArrayX[i], g_u8_ArrayY[0]);
+    }
+    g_b_snake_statue = false;
+  }
+#endif
+  for (i = 0; i < g_u8_Snake_Len; i++)
   {
     draw_snake(g_u8_ArrayX[i], g_u8_ArrayY[i]);
   }
@@ -151,20 +144,25 @@ void snke_move(void)
   default:
     break;
   }
+#if 1
   if((g_u8_Snake_Headx == g_u8_FoodX)&&(g_u8_Snake_Heady == g_u8_FoodY))
   {
     g_b_food_eaten = true;
     g_u8_Snake_Len++;
     g_u16_Score++;
     g_u16_level = g_u16_Score/5+1;
+    g_u16_SnakeSpeed -= g_u16_level;
   }
-  for(int i= g_u8_Snake_Len-1;i>0;i--)
+#endif
+  for(i= g_u8_Snake_Len-1;i>0;i--)
   {
     g_u8_ArrayX[i] = g_u8_ArrayX[i-1];
     g_u8_ArrayY[i] = g_u8_ArrayY[i-1];
   }
   g_u8_ArrayX[0] = g_u8_Snake_Headx;
   g_u8_ArrayY[0] = g_u8_Snake_Heady;
+  check_snake_die();
+
 }
 void draw_food(void)
 {
@@ -179,7 +177,7 @@ void draw_food(void)
       g_u8_FoodX = (uint8_t)(random(4, 100) / 4) * 4;
       g_u8_FoodY = (uint8_t)(random(4, 60) / 4) * 4;
 
-      for (int i = g_u8_Snake_Len - 1; i > 0; i--) // 遍历整个蛇身方块，若食物在蛇身内则重新生成
+      for (i = g_u8_Snake_Len - 1; i > 0; i--) // 遍历整个蛇身方块，若食物在蛇身内则重新生成
       {
         if (g_u8_FoodX == g_u8_ArrayX[i] && g_u8_FoodX == g_u8_ArrayY[i])
         {
@@ -191,8 +189,40 @@ void draw_food(void)
 
   g_b_food_eaten = false;
 }
+void check_snake_die(void)
+{
+  if(g_u8_Snake_Headx<4||g_u8_Snake_Headx>92||g_u8_Snake_Heady<1||g_u8_Snake_Heady>56)
+  {
+    g_b_game_over = true;
+  }
+  if(g_u8_Snake_Len>4)
+  {
+    for(int i=1;i<g_u8_Snake_Len;i++)
+    {
+      if(g_u8_Snake_Headx==g_u8_ArrayX[i]&&g_u8_Snake_Heady==g_u8_ArrayY[i])
+      {
+        g_b_game_over = true;
+      }
+    }
+  }
+}
+void draw_game_over()
+{
+  u8g2.clearBuffer();// 清屏
+  u8g2.setFont(u8g2_font_6x10_tf); // 设置字体大小
+  u8g2.setCursor(10, 10);// 设置显示位置
 
+  u8g2.print("GAME OVER");
+  u8g2.setCursor(30, 35);// 设置显示位置
+  u8g2.print("LEVE:");
+  u8g2.setCursor(30, 55);// 设置显示位置
+  u8g2.print("SCOR:");
 
+  show_score(80, 35, g_u16_level);
+  show_score(80, 55, g_u16_Score);
+
+  u8g2.sendBuffer();
+}
 /*user define function end*/
 
 
@@ -202,25 +232,32 @@ void setup(void) {
   Serial.begin(115200);
 
   Serial.println("test\n\r");
-  u8g2.setFont(u8g2_font_6x10_tf);
-  u8g2.drawStr(1,54,"github.com/olikraus/u8g2");
   
   key_init();
   attachInterrupt(KEY0, handleInterrupt_KEY0, RISING);
   attachInterrupt(KEY1, handleInterrupt_KEY1, RISING);
+  attachInterrupt(KEY2, handleInterrupt_KEY2, RISING);
+  attachInterrupt(KEY3, handleInterrupt_KEY3, RISING);
   randomSeed(analogRead(3));
+  g_u8_ArrayX[0]  = g_u8_Snake_Headx;
+  g_u8_ArrayY[0]  = g_u8_Snake_Heady;
 }
 
 
 void loop(void) {
   u8g2.clearBuffer();
-  u8g2.setFont(u8g2_font_6x10_tf);
-  
-  snke_move();
-  draw_food();
-  screen();
-  
-  //key_scan();
+  if(g_b_game_over)
+  {
+    draw_game_over();
+  }
+  else
+  {
+    snke_move();
+    draw_food();
+    screen();
+  }
+
+
   u8g2.sendBuffer();
-  delay(100);
+  delay(g_u16_SnakeSpeed);
 }
